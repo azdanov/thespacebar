a3eccdc055096eab2cc638d77948a126748ebce1
Anton Zdanov <azdanov@pm.me>
2018-08-30 18:32:41 +0300

    feat(js): add heart functionality

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function random_int;
 use function str_replace;
 use function ucwords;
 
@@ -26,19 +28,36 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}", name="article_show")
      *
-     * @param string $slug A slug for a news article
+     * @param string $slug Slug for a news article
      */
     public function show(string $slug) : Response
     {
         $comments = [
-            'Lorem ipsum dolor sit amet, consectetur adipisicing elit!',
-            'Lorem ipsum dolor sit amet.',
-            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, iure?',
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
         ];
 
         return $this->render(
             'article/show.html.twig',
-            ['title' => ucwords(str_replace('-', ' ', $slug)), 'comments' => $comments]
+            [
+                'title' => ucwords(str_replace('-', ' ', $slug)),
+                'comments' => $comments,
+                'slug' => $slug,
+            ]
         );
+    }
+
+    /**
+     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     *
+     * @param string $slug Slug for a news article
+     *
+     * @throws \Exception
+     */
+    public function toggleArticleHeart(string $slug) : JsonResponse
+    {
+        // TODO - actually heart/unheart the article!
+        return $this->json(['hearts' => random_int(5, 100)]);
     }
 }
