28874d8309176f4d97d83fa9f21419fdf2d4e746
Anton Zdanov <azdanov@pm.me>
2018-08-30 16:10:07 +0300

    feat(twig): add twig and article template

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,14 +4,16 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use function sprintf;
+use function str_replace;
+use function ucwords;
 
 /**
  * Class ArticleController.
  */
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route(path="/", methods={"GET"})
@@ -28,6 +30,15 @@ class ArticleController
      */
     public function show(string $slug) : Response
     {
-        return new Response(sprintf('Future page that shows an article: "%s"', $slug));
+        $comments = [
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit!',
+            'Lorem ipsum dolor sit amet.',
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, iure?',
+        ];
+
+        return $this->render(
+            'article/show.html.twig',
+            ['title' => ucwords(str_replace('-', ' ', $slug)), 'comments' => $comments]
+        );
     }
 }
