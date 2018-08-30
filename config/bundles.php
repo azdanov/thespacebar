56bd4c898597ec8875bdbf5695c549422139cb8a
Anton Zdanov <azdanov@pm.me>
2018-08-30 15:21:24 +0300

    feat(recipe): add twig

--- config/bundles.php
+++ config/bundles.php
@@ -1,7 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
