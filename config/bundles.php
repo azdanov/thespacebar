01b62377fae0d9dc76aae3884726ba5c45a8eec6
Anton Zdanov <azdanov@pm.me>
2018-08-30 16:27:50 +0300

    feat: add profiler

--- config/bundles.php
+++ config/bundles.php
@@ -1,10 +1,9 @@
 <?php
 
-declare(strict_types=1);
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
